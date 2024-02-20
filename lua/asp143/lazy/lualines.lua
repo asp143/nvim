6d928e450263c16b25a2142ea0b3dd8ae5ad2e3b
ralph <mungcs09@gmail.com>
2024-02-20 23:01:01 +0800

    Update to lazy

---
+++ lua/asp143/lazy/lualines.lua
@@ -0,0 +1,21 @@
+return {
+	config = function()
+		require('lualine').setup {
+			options = {
+				-- icons_enabled = false,
+				theme = 'onedark',
+				component_separators = '|',
+				section_separators = '',
+			},
+			sections = {
+				lualine_a = {
+					{
+						'filename',
+						path = 1,
+					}
+				}
+			}
+		}
+	end
+
+}

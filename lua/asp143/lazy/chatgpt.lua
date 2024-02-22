d3d90ec2472daeb6a4efc1fa1df6869b98edb680
mungcs09@gmail.com <mungcs09@gmail.com>
2024-02-22 19:08:04 +0800

    [Feature][Ralph] Added bw

--- lua/asp143/lazy/chatgpt.lua
+++ lua/asp143/lazy/chatgpt.lua
@@ -4,7 +4,8 @@ return {
 	config = function()
 		local home = vim.fn.expand("$HOME")
 		require("chatgpt").setup({
-			api_key_cmd = "gpg --decrypt " .. home .. "/.config/nvim/secret.txt.gpg"
+			-- api_key_cmd = "gpg --decrypt " .. home .. "/.config/nvim/secret.txt.gpg"
+			api_key_cmd = "bw get notes openai"
 		})
 	end,
 	dependencies = {

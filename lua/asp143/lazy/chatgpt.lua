f4f51b02dabd49ae359287ff2a514fb869269634
Ralph <ralph@symph.co>
2024-02-23 15:57:10 +0800

    Fixed boarders

--- lua/asp143/lazy/chatgpt.lua
+++ lua/asp143/lazy/chatgpt.lua
@@ -5,6 +5,21 @@ return {
 		require("chatgpt").setup({
 			-- api_key_cmd = "gpg --decrypt " .. home .. "/.config/nvim/secret.txt.gpg"
 			api_key_cmd = "bw get notes openai",
+			popup_window = {
+				border = {
+					highlight = "Normal",
+				},
+			},
+			system_window = {
+				border = {
+					highlight = "Normal",
+				},
+			},
+			popup_input = {
+				border = {
+					highlight = "Normal",
+				},
+			},
 		})
 	end,
 	dependencies = {

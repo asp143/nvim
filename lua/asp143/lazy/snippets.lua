0171156844ea0997d3508819e525a2d7a6e098e3
Ralph <mungcs09@gmail.com>
2024-04-27 16:10:17 +0800

    Added new version

--- lua/asp143/lazy/snippets.lua
+++ lua/asp143/lazy/snippets.lua
@@ -3,9 +3,9 @@ return {
 		"L3MON4D3/LuaSnip",
 		-- follow latest release.
 		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		build = "make install_jsregexp",
 
 		dependencies = { "rafamadriz/friendly-snippets" },
-		build = "make install_jsregexp",
 
 		config = function()
 			local ls = require("luasnip")
--- lua/asp143/lazy/undotree.lua
+++ lua/asp143/lazy/undotree.lua
@@ -1,8 +1,6 @@
 return {
 	"mbbill/undotree",
 	config = function()
-		vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-			{ silent = true, noremap = true }
-		)
-	end
+		vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+	end,
 }

2914f6c2423bf7d37c3f65bc5e9cba22e00f1d92
ralph <mungcs09@gmail.com>
2025-01-22 21:16:11 +0800

    Updated

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -5,5 +5,11 @@ return {
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.coding.copilot" },
+  { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
 }

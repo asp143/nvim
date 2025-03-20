23cb3422ce9a266383c7adeb1d97386a33d83587
ralph <mungcs09@gmail.com>
2025-03-20 17:25:12 +0800

    Updated plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },

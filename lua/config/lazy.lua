87d1f38c324fce040a0affb33ba1cb9c037469d2
ralph <mungcs09@gmail.com>
2025-04-12 18:58:19 +0800

    feat: added php and md

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,6 +33,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
+    { import = "lazyvim.plugins.extras.lang.php" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "plugins" },
   },
   defaults = {

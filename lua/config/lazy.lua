5bdb374f4d2165a95352dafcdd7a7ce8950ce6d3
ralph <mungcs09@gmail.com>
2025-04-13 18:03:33 +0800

    feat: added Zig

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.zig" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
     { import = "lazyvim.plugins.extras.ai.copilot" },

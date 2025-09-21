09129d7e411fa9cce631c24699f1c0270e4a8c9c
ralph <mungcs09@gmail.com>
2025-09-22 02:12:20 +0800

    feat: Added openai

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,8 +6,6 @@ return {
     or "make",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
-  ---@module 'avante'
-  ---@type avante.Config
   opts = {
     -- add any opts here
     -- this file can contain specific instructions for your project
@@ -35,6 +33,15 @@ return {
       claude = {
         endpoint = "https://api.anthropic.com",
         model = "claude-opus-4-20250514",
+        timeout = 30000, -- Timeout in milliseconds
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 20480,
+        },
+      },
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-5",
         timeout = 30000, -- Timeout in milliseconds
         extra_request_body = {
           temperature = 0.75,

38feb965025a8476e14245395cb7c801520e22f6
ralph <mungcs09@gmail.com>
2026-01-07 18:06:02 +0800

    feat: Updated config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,53 +1,33 @@
---leader Options are automatically loaded before lazy.nvim startup
+-- leader Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- [[ Setting options ]]
--- See `:help vim.o`
 
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = "a"
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.wo.signcolumn = "yes"
-vim.wo.relativenumber = true
-
----
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+vim.opt.breakindent = true
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.updatetime = 50
+vim.opt.signcolumn = "yes"
 
+vim.opt.scrolloff = 8
+vim.opt.isfname:append("@-@")
 vim.opt.colorcolumn = "80"
 
--- Set colorscheme
-vim.o.termguicolors = true
-vim.o.background = "dark" -- or "light" for light mode
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
 
--- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
+vim.opt.completeopt = "menuone,noselect"
+
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-
 vim.g.snacks_animate = false
